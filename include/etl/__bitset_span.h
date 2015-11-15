bc59a45cd8c5b96a4ad6e903acf205566d11ba1e
rukkal <kean.mariotti@gmail.com>
2015-11-15 12:20:11 +0100

    added bitset_span

---
+++ include/etl/__bitset_span.h
@@ -0,0 +1,157 @@
+/*
+Copyright © 2015 Kean Mariotti <kean.mariotti@gmail.com>
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+*/
+
+#ifndef __SSTL_BITSET_SPAN__
+#define __SSTL_BITSET_SPAN__
+
+#include <cassert>
+#include <limits>
+
+namespace etl
+{
+// A GSL-like (Guideline Support Library) implementation to manipulate a
+// span of bits. This class is leveraged by other components to reduce
+// code bloat avoiding the use of a template size parameter.
+class bitset_span
+{
+public:
+   bitset_span(void* data, size_t size)
+   : blocks(static_cast<block_type*>(data))
+   , num_of_bits(size)
+   {
+      assert(num_of_bits > 0);
+   }
+
+   void set(size_t idx)
+   {
+      assert(idx < num_of_bits);
+      auto* byte = get_block(idx);
+      auto bit_idx = get_bit_idx(idx);
+      *byte |= (1<<bit_idx);
+   }
+
+   void set()
+   {
+      // entire blocks
+      auto p = blocks;
+      auto end = p+get_num_of_blocks()-1;
+      while(p!=end)
+      {
+         *p++ = std::numeric_limits<block_type>::max();
+      }
+      // remaining bits
+      for(size_t idx = (get_num_of_blocks()-1)*sizeof(block_type); idx<num_of_bits; ++idx)
+      {
+         set(idx);
+      }
+   }
+
+   void reset(size_t idx)
+   {
+      assert(idx < num_of_bits);
+      auto* byte = get_block(idx);
+      auto bit_idx = get_bit_idx(idx);
+      *byte &= ~(1<<bit_idx);
+   }
+
+   void reset()
+   {
+      // entire blocks
+      auto p = blocks;
+      auto end = p+get_num_of_blocks()-1;
+      while(p!=end)
+      {
+         *p++ = 0;
+      }
+      // remaining bits
+      for(size_t idx = (get_num_of_blocks()-1)*sizeof(block_type); idx<num_of_bits; ++idx)
+      {
+         reset(idx);
+      }
+   }
+
+   bool test(size_t idx) const
+   {
+      assert(idx < num_of_bits);
+      auto* byte = get_block(idx);
+      auto bit_idx = get_bit_idx(idx);
+      return ((*byte) & (1<<bit_idx)) != 0;
+   }
+
+   bool all() const
+   {
+      // entire blocks
+      auto p = blocks;
+      auto end = p+get_num_of_blocks()-1;
+      while(p!=end)
+      {
+         if(*p++ != std::numeric_limits<block_type>::max())
+            return false;
+      }
+      // remaining bits
+      for(size_t idx = (get_num_of_blocks()-1)*sizeof(block_type); idx<num_of_bits; ++idx)
+      {
+         if(!test(idx))
+            return false;
+      }
+      return true;
+   }
+
+   size_t size() const { return num_of_bits; }
+
+   size_t count() const
+   {
+      size_t _count = 0;
+      for(size_t i=0; i<num_of_bits; ++i)
+      {
+         if(test(i))
+            ++_count;
+      }
+      return _count;
+   }
+
+private:
+   using block_type = unsigned char;
+   using const_block_type = unsigned char;
+
+private:
+   size_t get_byte_idx(size_t idx) const
+   {
+      return idx / (8 * sizeof(block_type));
+   }
+
+   block_type* get_block(size_t idx)
+   {
+      auto byte_idx = get_byte_idx(idx);
+      return blocks + byte_idx;
+   }
+
+   const_block_type* get_block(size_t idx) const
+   {
+      auto byte_idx = get_byte_idx(idx);
+      return blocks + byte_idx;
+   }
+
+   size_t get_bit_idx(size_t idx) const
+   {
+      auto byte_idx = get_byte_idx(idx);
+      auto bit_idx = idx - 8 * byte_idx;
+      return bit_idx;
+   }
+
+   size_t get_num_of_blocks() const
+   {
+      return (num_of_bits-1) / (8 * sizeof(block_type)) + 1;
+   }
+
+public:
+   block_type* blocks;
+   size_t num_of_bits;
+};
+}
+
+#endif
---
+++ test/test_bitset_span.cpp
@@ -0,0 +1,120 @@
+/*
+Copyright © 2015 Kean Mariotti <kean.mariotti@gmail.com>
+This work is free. You can redistribute it and/or modify it under the
+terms of the Do What The Fuck You Want To Public License, Version 2,
+as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
+*/
+
+#include <UnitTest++/UnitTest++.h>
+#include <bitset>
+#include <array>
+#include <etl/__bitset_span.h>
+
+namespace test_etl
+{
+SUITE(test_bitset_span)
+{
+   template<class T1, class T2>
+   void check_equal(const T1& t1, const T2& t2)
+   {
+      CHECK_EQUAL(t1.size(), t2.size());
+      CHECK_EQUAL(t1.count(), t2.count());
+      for(size_t i=0; i<t1.size(); ++i)
+      {
+         CHECK_EQUAL(t1.test(i), t2.test(i));
+      }
+   }
+
+   TEST(test_constructor)
+   {
+      auto expected = std::bitset<31> {};
+      std::array<char, 5> actual_data;
+      auto actual = etl::bitset_span(actual_data.data(), 31);
+      check_equal(expected, actual);
+   }
+
+   TEST(test_set)
+   {
+      auto expected = std::bitset<31> {};
+      std::array<char, 5> actual_data;
+      auto actual = etl::bitset_span(actual_data.data(), 31);
+
+      expected.set(0);
+      actual.set(0);
+      check_equal(expected, actual);
+
+      expected.set(1);
+      actual.set(1);
+      check_equal(expected, actual);
+
+      expected.set(29);
+      actual.set(29);
+      check_equal(expected, actual);
+
+      expected.set(30);
+      actual.set(30);
+      check_equal(expected, actual);
+
+      expected.set(11);
+      actual.set(11);
+      check_equal(expected, actual);
+
+      expected.set();
+      actual.set();
+      check_equal(expected, actual);
+   }
+
+   TEST(test_reset)
+   {
+      auto expected = std::bitset<31> {};
+      std::array<char, 5> actual_data;
+      auto actual = etl::bitset_span(actual_data.data(), 31);
+
+      expected.set();
+      actual.set();
+      check_equal(expected, actual);
+
+      expected.reset(0);
+      actual.reset(0);
+      check_equal(expected, actual);
+
+      expected.reset(1);
+      actual.reset(1);
+      check_equal(expected, actual);
+
+      expected.reset(29);
+      actual.reset(29);
+      check_equal(expected, actual);
+
+      expected.reset(30);
+      actual.reset(30);
+      check_equal(expected, actual);
+
+      expected.reset(11);
+      actual.reset(11);
+      check_equal(expected, actual);
+
+      expected.reset();
+      actual.reset();
+      check_equal(expected, actual);
+   }
+
+   TEST(test_all)
+   {
+      std::array<char, 5> actual_data;
+      auto actual = etl::bitset_span(actual_data.data(), 31);
+
+      CHECK_EQUAL(false, actual.all());
+      actual.set();
+      CHECK_EQUAL(true, actual.all());
+      actual.reset(0);
+      CHECK_EQUAL(false, actual.all());
+      actual.set(0);
+      CHECK_EQUAL(true, actual.all());
+      actual.reset(30);
+      CHECK_EQUAL(false, actual.all());
+      actual.set(30);
+      CHECK_EQUAL(true, actual.all());
+   }
+};
+}
