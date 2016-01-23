bd77ca195bfce4fab120cd5aaac1b17e7247afcc
rukkal <kean.mariotti@gmail.com>
2016-01-23 12:54:05 +0100

    noexcept expression evaluates always to true when exceptions are turned off (e.g. -fno-exceptions)

--- include/sstl/__internal/_except.h
+++ include/sstl/__internal/_except.h
@@ -18,6 +18,9 @@ as published by Sam Hocevar. See http://www.wtfpl.net/ for more details.
 #if IS_MSVC() && _MSC_VER < 1900
    #define _sstl_noexcept_
    #define _sstl_noexcept(...)
+#elif !_sstl_has_exceptions()
+   #define _sstl_noexcept_ noexcept
+   #define _sstl_noexcept(...) noexcept
 #else
    #define _sstl_noexcept_ noexcept
    #define _sstl_noexcept(...) noexcept(__VA_ARGS__)
