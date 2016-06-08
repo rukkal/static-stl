ee624d5da91b0e4662681c9f6f670a4615b37702
rukkal <kean.mariotti@gmail.com>
2016-06-08 18:39:46 +0200

    toolchain files: added -Wpedantic and -Werror

--- toolchain-clang.cmake
+++ toolchain-clang.cmake
@@ -1,11 +1,11 @@
 
-SET(CMAKE_CXX_FLAGS "-Wall -std=c++11" CACHE STRING "Common flags for C++ compiler")
+SET(CMAKE_CXX_FLAGS "-Wall -Wpedantic -Werror -std=c++11" CACHE STRING "Common flags for C++ compiler")
 SET(CMAKE_CXX_FLAGS_DEBUG "-g3" CACHE STRING "Debug flags for C++ compiler")
 SET(CMAKE_CXX_FLAGS_RELEASE "-DNDEBUG -O3" CACHE STRING "Release flags for C++ compiler")
 SET(CMAKE_CXX_FLAGS_MINSIZEREL "-DNDEBUG -Os" CACHE STRING "Minimum size release flags for C++ compiler")
 SET(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-DNDEBUG -Og -g3" CACHE STRING "Release with debug info flags for C++ compiler")
 
-SET(CMAKE_C_FLAGS "-Wall" CACHE STRING "Common flags for C compiler")
+SET(CMAKE_C_FLAGS "-Wall -Wpedantic -Werror" CACHE STRING "Common flags for C compiler")
 SET(CMAKE_C_FLAGS_DEBUG "-g3" CACHE STRING "Debug flags for C compiler")
 SET(CMAKE_C_FLAGS_RELEASE "-DNDEBUG -O3" CACHE STRING "Release flags for C compiler")
 SET(CMAKE_C_FLAGS_MINSIZEREL "-DNDEBUG -Os" CACHE STRING "Minimum size release flags for C compiler")
