b5c9dd26778fc3669dc178f6863addd432b2263b
rukkal <kean.mariotti@gmail.com>
2016-07-30 11:10:02 +0200

    fixed travis build (msan runs with instrumented libc++)

--- toolchain_files/clang-msan.cmake
+++ toolchain_files/clang-msan.cmake
@@ -1,7 +1,7 @@
 
 include("${CMAKE_CURRENT_LIST_DIR}/clang.cmake")
 
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fsanitize=memory -fsanitize-memory-track-origins -fsanitize-memory-use-after-dtor")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -stdlib=libc++ -fsanitize=memory -fsanitize-memory-track-origins -fsanitize-memory-use-after-dtor")
 
-set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -fsanitize=memory -fsanitize-memory-track-origins -fsanitize-memory-use-after-dtor")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -stdlib=libc++ -fsanitize=memory -fsanitize-memory-track-origins -fsanitize-memory-use-after-dtor")
 
